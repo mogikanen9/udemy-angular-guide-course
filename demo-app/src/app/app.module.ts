7390b3309d6ae2f45afdd71b6bb34f663e203dfc
Vlad Voinov <mogikanensoftware@gmail.com>
2020-09-23 14:39:50 -0400

    header, shopping, recipe components

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
