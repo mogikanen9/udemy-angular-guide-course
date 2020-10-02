7411c6ca02b7c91ae7a12ccf43e34473ec4b9f74
Vlad Voinov <mogikanensoftware@gmail.com>
2020-10-02 10:03:27 -0400

    add dynamic controls to project form

--- demo-app/src/app/project/project.component.ts
+++ demo-app/src/app/project/project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-project',
@@ -9,17 +10,48 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ProjectComponent implements OnInit {
 
   projectForm: FormGroup;
+  projectStatuses = [
+    { key: 'na', value: 'Please, select' },
+    { key: 'in-progress', value: 'In Progress' },
+    { key: 'complete', value: 'Complete' }];
+
+  questionValues = ['How old are you?', 'What is you place fo birth?', 'What is your name?'];
 
   constructor() { }
 
   ngOnInit(): void {
     this.projectForm = new FormGroup({
-      'name': new FormControl('Please, provide name', [Validators.required, Validators.min(2)]),
-      'email': new FormControl(null, [Validators.required, Validators.email])
+      'name': new FormControl(null, [Validators.required, Validators.min(2)], [this.forbiddenProjectName]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('in-progress'),
+      'questions': new FormArray([ new FormControl(null)])
     });
+  }
+
+  get myquestions(): FormArray {
+    return this.projectForm.get('questions') as FormArray;
   }
 
   onSubmit(): void {
     console.log('this.projectForm.value->', this.projectForm.value);
+  }
+
+  forbiddenProjectName(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ 'projectNameIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+  }
+
+  addQuestion(): void {
+    const myQ = this.myquestions;
+    if (this.myquestions.length < this.questionValues.length) {
+      myQ.push(new FormControl(null));
+    }
   }
 }
