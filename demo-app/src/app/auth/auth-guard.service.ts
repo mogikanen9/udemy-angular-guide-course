4bc50861a61cc946984bd5a162db319b7936304a
Vlad Voinov <mogikanensoftware@gmail.com>
2020-10-14 18:20:22 -0400

    authguard with redirect with take

--- demo-app/src/app/auth/auth-guard.service.ts
+++ demo-app/src/app/auth/auth-guard.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { MyAuthService } from './auth.service';
 
 @Injectable()
@@ -11,9 +11,11 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
         boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.authService.userSubject.pipe(map(user => {
-            const isAUth = !!user;
-            if(isAUth){
+        return this.authService.userSubject.pipe(
+            take(1),
+            map(user => {
+            const isAuth = !!user;
+            if(isAuth){
                 return true;
             }else{
                 return this.router.createUrlTree(['/auth']);
